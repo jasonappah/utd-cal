2fa4e660d3fb427e718f99d8087cb408629041a3
Jason Antwi-Appah <hey@jasonaa.me>
2024-01-02 13:29:12 -0600

    Add new data

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-export const fall2023holidays = [
+const fall2023holidays = [
     {
       "holiday": "Labor Day",
       "date": "2023-09-04"
@@ -32,3 +32,37 @@ export const fall2023holidays = [
       "date": "2023-11-26"
     }
   ]
+
+const spring2024holidays = [
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-11"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-12"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-13"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-14"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-15"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-16"
+  },
+  {
+    "holiday": "Spring break",
+    "date": "2024-03-17"
+  }
+]
+
+export const currentHolidays = spring2024holidays
+export const UTD_TIMEZONE = 'America/Chicago'
